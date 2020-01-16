74da8abdf2f26398256e144d5ef41d437124ed0d
Robert J. Mitchell <robert.judson@outlook.com>
2020-01-15 19:36:14 -0500

    still not changing color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,10 @@
 import React, { useState } from 'react';
 
-import useDarkMode from '../hooks/useDarkMode';
+import { useDarkMode } from '../hooks/useDarkMode';
 
 const Navbar = () => {
-  const [darkMode, setDarkMode] = useState(true);
-  const toggleMode = e => {
-    e.preventDefault();
-    setDarkMode(!darkMode);
-  };
+  const [darkMode, toggleMode] = useDarkMode('dark', false);
+  
 
   return (
     <nav className="navbar">
--- src/hooks/useDarkMode.jsx
+++ src/hooks/useDarkMode.jsx
@@ -1,14 +1,23 @@
 
+import { useEffect } from 'react';
 import { useLocalStorage } from './useLocalStorage';
 
-export const useDarkMode = (key) => {
-  const [values, setValues] = useLocalStorage(key);
+export const useDarkMode = (key, initialValue) => {
+  const [darkMode, setDarkMode] = useLocalStorage(key, initialValue);
 
-  useEffect(
-    () => {
-      if(useLocalStorage.value === true){
-        return 
+  const toggleMode = e => {
+    e.preventDefault();
+    setDarkMode(!darkMode);
+  }
+  useEffect(() => {
+      if(darkMode === false){
+        document.querySelector('body').classList.remove = ('dark-mode');
+      }if(darkMode === true){
+        document.querySelector('body').classList.toggle = ('dark-mode');
       }
-    }
+    },
+    [darkMode],
   )
+
+  return [darkMode, toggleMode]
 }
